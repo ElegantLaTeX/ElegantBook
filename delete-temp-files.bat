fbb6a6f124df9c99176c888120fde6625e2d14ea
EthanDeng <ddswhu@outlook.com>
2019-05-06 12:35:40 +0800

    remove pdf

---
+++ delete-temp-files.bat
@@ -0,0 +1,5 @@
+@echo off
+del /q *.aux *.bbl *.blg *.log *.out *.toc *.bcf *.xml *.synctex *.nlo *.nls *.bak *.ind *.idx *.ilg *.lof *.lot *.ent-x *.tmp *.ltx *.los *.lol *.loc *.listing *.gz *.userbak *.nav *.snm *.vrb *.synctex(busy)
+
+
+del /q *.nav *.snm *.vrb *.fls *.xdv *.fdb_latexmk
